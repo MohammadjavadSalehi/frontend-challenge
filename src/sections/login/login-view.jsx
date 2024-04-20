0bd784367a69d5ac6f3a244467dcc5ff035076c2
Mohammadjavad Salehi <83529512+MohammadjavadSalehi@users.noreply.github.com>
2024-04-20 17:26:46 +0330

    add login functions

--- src/sections/login/login-view.jsx
+++ src/sections/login/login-view.jsx
@@ -20,8 +20,19 @@ import Iconify from 'src/components/iconify';
 // ----------------------------------------------------------------------
 
 export default function LoginView() {
-  const [email, setEmail] = useState('');
-  const [pass, setPass] = useState('');
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
   const theme = useTheme();
 
@@ -29,70 +40,49 @@ export default function LoginView() {
 
   const [showPassword, setShowPassword] = useState(false);
 
-  const handleClick = () => {
-    router.push('/articles');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formErrors = {};
+    if (!formData.email.trim()) {
+      formErrors.email = 'Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      formErrors.email = 'Email is invalid';
+    }
+    if (!formData.password.trim()) {
+      formErrors.password = 'Password is required';
+    }
+    if (Object.keys(formErrors).length > 0) {
+      setErrors(formErrors);
+    } else {
+      await fetch('https://api.realworld.io/api/users/login', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ user: formData }),
+      })
+        .then(async (r) => {
+          const data = await r.json();
+          if (r.ok) {
+            localStorage.setItem('user', JSON.stringify(data.user));
+            setErrors({});
+            router.push('/articles');
+          } else {
+            const serverErrors = {};
+            Object.keys(data.errors).forEach((key) => {
+              serverErrors[key] = data.errors[key].join(', ');
+            });
+            setErrors(serverErrors);
+            alert(r.errors.email);
+            formErrors.password = r.errors.password;
+          }
+        })
+        .catch((r) => {
+          console.log(r);
+        });
+    }
   };
-
-  const renderForm = (
-    <>
-      <form action="" onSubmit={handleClick}>
-        <Stack spacing={3} sx={{ mt: 4 }}>
-          <TextField
-            required
-            name="email"
-            label="Email"
-            type="email"
-            value={email}
-            error={email.length === 0}
-            helperText={!email.length ? 'Required field' : ''}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-
-          <TextField
-            required
-            name="password"
-            label="Password"
-            value={pass}
-            error={pass.length === 0}
-            helperText={!pass.length ? 'Required field' : ''}
-            onChange={(e) => {
-              setPass(e.target.value);
-            }}
-            type={showPassword ? 'text' : 'password'}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
-                    <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
-        </Stack>
-
-        <LoadingButton
-          fullWidth
-          size="large"
-          type="submit"
-          variant="contained"
-          color="primary"
-          sx={{ mt: 3 }}
-        >
-          Login
-        </LoadingButton>
-      </form>
-
-      <Typography variant="body2" sx={{ mt: 2 }}>
-        Don’t have an account?
-        <Link variant="subtitle2" href="/register" sx={{ ml: 0.5 }}>
-          Register now
-        </Link>
-      </Typography>
-    </>
-  );
 
   return (
     <Box
@@ -116,7 +106,55 @@ export default function LoginView() {
             <Typography variant="h3">Login</Typography>
           </center>
 
-          {renderForm}
+          <form onSubmit={handleSubmit}>
+            <Stack spacing={3} sx={{ mt: 4 }}>
+              <TextField
+                name="email"
+                label="Email"
+                type="email"
+                value={formData.email}
+                onChange={handleChange}
+                error={!!errors.email}
+                helperText={errors.email}
+              />
+              <TextField
+                name="password"
+                label="Password"
+                value={formData.password}
+                onChange={handleChange}
+                error={!!errors.password}
+                helperText={errors.password}
+                type={showPassword ? 'text' : 'password'}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                        <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Stack>
+
+            <LoadingButton
+              fullWidth
+              size="large"
+              type="submit"
+              variant="contained"
+              color="primary"
+              sx={{ mt: 3 }}
+            >
+              Login
+            </LoadingButton>
+          </form>
+
+          <Typography variant="body2" sx={{ mt: 2 }}>
+            Don`t have account?
+            <Link variant="subtitle2" href="/register" sx={{ ml: 0.5 }}>
+              Register now
+            </Link>
+          </Typography>
         </Card>
       </Stack>
     </Box>
